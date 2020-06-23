e864dc36456b022c73bb59f89320cf4b5c3c931d
lewisj44 <jlewis4@aggies.ncat.edu>
2020-06-23 15:13:55 -0400

    add tool

---
+++ testtool.rb
@@ -0,0 +1,12 @@
+class Testtool < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/lewisj44/testtool/archive/v1.0.1.tar.gz"
+  sha256 "57cc33d8cf6f697d5acd140a57868b4173591e7c9747fb90d285ae2c902eb9bc"
+
+ 
+
+  def install
+    bin.install "testtool"
+
+end
